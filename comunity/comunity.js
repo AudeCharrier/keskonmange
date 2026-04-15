92ebae4a02282b5e56863affd080ae9303364fec
AUDE CHARRIER <charrier.aude@gmail.com>
2026-04-15 15:02:28 +0200

    worked on burger navbar

--- comunity/comunity.js
+++ comunity/comunity.js
@@ -1,1 +1,13 @@
+const burgerIcon = document.getElementById("burgerIcon");
+const navUl = document.querySelector(".navbar ul");
+const navbar = document.querySelector(".navbar");
 
+const hauteur = navbar.getBoundingClientRect().height;
+document
+	.querySelector(":root")
+	.style.setProperty("--navbar-height", hauteur + "px");
+
+burgerIcon.addEventListener("click", () => {
+	navUl.classList.toggle("active");
+	burgerIcon.classList.toggle("active");
+});
